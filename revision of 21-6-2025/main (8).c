15c2ec4f056280cce5923a41fd913f84bf80bb09
aayushgevariya <aayushgevariya22@gmail.com>
2025-06-23 11:14:51 +0530

    Add files via upload

---
+++ revision of 21-6-2025/main (8).c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+
+{
+
+    int marks[5];
+
+    int i,sum=0,avg;
+
+    for(i=0;i<5;i++)
+
+    {
+
+        printf("\n Enter Your Marks");    
+
+        scanf("%d",&marks[i]);
+
+    }
+    
+     for(i=0;i<5;i++)
+
+    {
+
+         sum+= marks[i];
+ 
+     }
+
+     avg = sum/5;
+
+     printf("\n Your Percentage is %d",avg);
+    
+    
+    getch();
+}
